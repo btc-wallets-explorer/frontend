6e2d31548bbc110d9bf2c47f1e8cd5ea48345a98
prometheanSolutions <promethean.solutions@proton.me>
2023-01-08 19:29:06 +0100

    progress

--- electrum.js
+++ electrum.js
@@ -1,37 +1,39 @@
 var net = require('net');
+var util = require('util');
+
 
 var client = new net.Socket();
-client.connect(50001, '127.0.0.1');
+client.connect(50001, 'localhost');
 
-function get(method, scriptHash, callback) {
-	    let rpc = {"jsonrpc": "2.0", "method": "blockchain.scripthash." + method, "id": 0, "params": [scriptHash]};
-	      client.write(JSON.stringify(rpc) + '\n');
-	      client.write(JSON.stringify(rpc) + '\n');
-	      client.write(JSON.stringify(rpc) + '\n');
-	      client.write(JSON.stringify(rpc) + '\n');
-	      client.end();
-
-}
-
-function getTransaction(transaction, callback) {
-    var client = new net.Socket();
-    client.connect(50001, '127.0.0.1', function() {
-
-        client.on('data', function(data) {
-            callback(JSON.parse(data).result);
-        });
-
-	    let rpc = {"jsonrpc": "2.0", "method": "blockchain.transaction.get", "id": 0, "params": [transaction, true]};
-	    client.write(JSON.stringify(rpc));
-	    client.end();
+async function get(method, scriptHash) {
+    return new Promise((resolve, reject) => {
+        client.on('data', data =>
+                  resolve((data + '')
+                          .split('\n')
+                          .filter(x => x !== '')
+                          .map(x => JSON.parse(x).result)[0])
+                 );
+        let rpc = {"jsonrpc": "2.0", "method": "blockchain.scripthash." + method, "id": 0, "params": [scriptHash]};
+        client.write(JSON.stringify(rpc) + '\n');
     });
+}
 
+async function getTransaction(transaction) {
+    return new Promise((resolve, reject) => {
+        client.on('data', data =>
+                  resolve((data + '')
+                          .split('\n')
+                          .filter(x => x !== '')
+                          .map(x => JSON.parse(x).result)[0])
+                 );
+        let rpc = {"jsonrpc": "2.0", "method": "blockchain.transaction.get", "id": 0, "params": [transaction, true]};
+        client.write(JSON.stringify(rpc) + '\n');
+    });
 }
 
 
-exports.getHistory = (scriptHash, callback) => get("get_history", scriptHash, callback);
-exports.getBalance = (scriptHash, callback) => get("get_balance", scriptHash, callback);
-exports.listUnspent = (scriptHash, callback) => get("listunspent", scriptHash, callback);
+exports.getHistory = (scriptHash, callback) => get("get_history", scriptHash);
+exports.getBalance = (scriptHash, callback) => get("get_balance", scriptHash);
+exports.listUnspent = (scriptHash, callback) => get("listunspent", scriptHash);
 
 exports.getTransaction = getTransaction;
-exports.close = function () {client.destroy();};
--- index.js
+++ index.js
@@ -1,76 +1,68 @@
-const fs = require('fs');
+async function main() {
+    const fs = require('fs');
 
-const WebSocket = require('ws');
+    const WebSocket = require('ws');
 
-const btc = require('./bitcoin');
-const electrum = require('./electrum');
-const helpers = require('./helpers');
+    const btc = require('./bitcoin');
+    const electrum = require('./electrum');
+    const helpers = require('./helpers');
 
-// load wallets
-let wallets;
-try {
-    const data = fs.readFileSync('./wallets.json', 'utf8');
-    wallets = JSON.parse(data);
-} catch (err) {
-    console.log(`Error reading file from disk: ${err}`);
-}
+    // load wallets
+    let wallets;
+    try {
+        const data = fs.readFileSync('./wallets.json', 'utf8');
+        wallets = JSON.parse(data);
+    } catch (err) {
+        console.log(`Error reading file from disk: ${err}`);
+    }
 
-const server = new WebSocket.Server({
-    port: 8080
-});
+    const server = new WebSocket.Server({
+        port: 8080
+    });
 
-const xpubs = wallets['multi'].xpubs;
+    const xpubs = wallets['multi'].xpubs;
 
-let addresses = helpers.range(100).map(index => btc.getAddressForMultisig(xpubs, index));
-let scriptHashes = addresses.map(a => btc.toScriptHash(a));
+    var addresses = helpers.range(100).map(index => btc.getAddressForMultisig(xpubs, index));
+    var scriptHashes = addresses.map(a => btc.toScriptHash(a));
+    var address = addresses[0];
+    var scriptHash = scriptHashes[0];
+    console.log(histories);
 
-let address = addresses[0];
-let scriptHash = scriptHashes[0];
+    var histories = [];
+    for (h of scriptHashes) {
+        var hist = await electrum.getHistory(h);
 
-electrum.getHistory(scriptHash, console.log);
+        if (hist.length > 0) {
+            histories.push(hist);
+        }
+    }
 
-// let histories = [];
-// addresses.map(a => electrum.getHistory(a, h => histories.push(h)));
+    console.log(histories);
+    var transactions = histories.map(h => h.map(t => t.tx_hash));
+    var transactions = await Promise.all(histories.map(h => h.map(t => electrum.getTransaction(t))));
 
+    var data =
+        {
+            "name": "Top Level",
+            "children": [
+                {
+                    "name": "Level 2: A",
+                    "children": [
+                        { "name": "Son of A" },
+                        { "name": "Daughter of A" }
+                    ]
+                },
+                { "name": "Level 2: B" }
+            ]
+        };
+    // electrum.getHistory(scriptHash, x => console.log(x));
+    server.on('connection', function(socket) {
+        socket.on('message', function(msg) {
+            electrum.getHistory(scriptHash, x => socket.send(JSON.stringify(data)));
+        });
+    });
 
-// let history;
-// let transaction;
-// electrum.getHistory(scriptHash, h => history = h);
-
-// electrum.getTransaction(history[0].tx_hash, console.log);
-// electrum.getTransaction(history[0].tx_hash, t => transaction = t);
-
-// console.log(addresses);
-// console.log(scriptHashes);
-
-// electrum.getTransaction("0489a9911d17f146545fe6ac3a9523057fff8c06fae4aa9b49bca8317919f81b", x => console.log(x));
-
-// electrum.getTransaction("0489a9911d17f146545fe6ac3a9523057fff8c06fae4aa9b49bca8317919f81b",
-//                         x => console.log(x.vin[0].scriptSig));
-// electrum.getTransaction("0489a9911d17f146545fe6ac3a9523057fff8c06fae4aa9b49bca8317919f81b",
-//                         x => console.log(x.vout[0].scriptPubKey));
-
-// var data =
-//     {
-//         "name": "Top Level",
-//         "children": [
-//             {
-//                 "name": "Level 2: A",
-//                 "children": [
-//                     { "name": "Son of A" },
-//                     { "name": "Daughter of A" }
-//                 ]
-//             },
-//             { "name": "Level 2: B" }
-//         ]
-//     };
-// electrum.getHistory(scriptHash, x => console.log(x));
-// server.on('connection', function(socket) {
-//     socket.on('message', function(msg) {
-//         electrum.getHistory(scriptHash, x => socket.send(JSON.stringify(data)));
-//     });
-// });
-
-
+};
+main();
 
 
