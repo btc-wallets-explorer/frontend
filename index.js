2b219cbf6aea6c45c5f7be3e9f8e5d1127642949
prometheanSolutions <promethean.solutions@proton.me>
2023-01-16 18:46:40 +0100

    add value to sankey graph

--- index.js
+++ index.js
@@ -35,56 +35,66 @@ async function main() {
     const transactionMap = Object.fromEntries(transactions.map(t => [t.txid, t]))
     const ourTransactions = Object.keys(transactionMap);
 
-    const links = transactions
+    const getTransaction = async (txid) => {
+        if (!txid in transactionMap)
+            transactionMap[txid] = await electrum.blockchainTransaction_get(txid, true);
+
+        return transactionMap[txid];
+    }
+
+    const links = await Promise.all(transactions
         .flatMap(t => t.vin.map(vin => ({ target: t.txid, source: vin.txid, value: 1, vin })))
-        .filter(l => ourTransactions.includes(l.target) && ourTransactions.includes(l.source));
-
-    console.log(Object.keys(transactions).length);
-
-    let id = 0;
-    let nodes = Object.values(transactionMap).map(tx => ({ id: id++, name: tx.txid, tx }));
+        .filter(l => ourTransactions.includes(l.target) && ourTransactions.includes(l.source))
+        .map(async l => ({...l, value: (await getTransaction(l.vin.txid)).vout[l.vin.vout].value})));
 
 
-    const model = { nodes, links };
-    console.log(model);
-    // console.log(model);
-    // const model = {
-    //     "nodes": [
-    //         { "node": 0, "name": "node0" },
-    //         { "node": 1, "name": "node1" },
-    //         { "node": 2, "name": "node2" },
-    //         { "node": 3, "name": "node3" },
-    //         { "node": 4, "name": "node4" }
-    //     ],
-    //     "links": [
-    //         { "source": 0, "target": 2, "value": 2 },
-    //         { "source": 1, "target": 2, "value": 2 },
-    //         { "source": 1, "target": 3, "value": 2 },
-    //         { "source": 0, "target": 4, "value": 2 },
-    //         { "source": 2, "target": 3, "value": 2 },
-    //         { "source": 2, "target": 4, "value": 2 },
-    //         { "source": 3, "target": 4, "value": 4 }
-    //     ]
-    // };
+
+console.log(Object.keys(transactions).length);
+
+let id = 0;
+let nodes = Object.values(transactionMap).map(tx => ({ id: id++, name: tx.txid, tx }));
 
 
-    const wss = new WebSocket.Server({ port: 8080 })
+const model = { nodes, links };
+console.log(model);
+// console.log(model);
+// const model = {
+//     "nodes": [
+//         { "node": 0, "name": "node0" },
+//         { "node": 1, "name": "node1" },
+//         { "node": 2, "name": "node2" },
+//         { "node": 3, "name": "node3" },
+//         { "node": 4, "name": "node4" }
+//     ],
+//     "links": [
+//         { "source": 0, "target": 2, "value": 2 },
+//         { "source": 1, "target": 2, "value": 2 },
+//         { "source": 1, "target": 3, "value": 2 },
+//         { "source": 0, "target": 4, "value": 2 },
+//         { "source": 2, "target": 3, "value": 2 },
+//         { "source": 2, "target": 4, "value": 2 },
+//         { "source": 3, "target": 4, "value": 4 }
+//     ]
+// };
 
-    wss.on("connection", ws => {
-        console.log("new client connected");
-        ws.on("message", data => {
-            console.log(`Client has sent us: ${data}`);
 
-            ws.send(JSON.stringify(model));
-        });
-        ws.on("close", () => {
-            console.log("the client has connected");
-        });
-        ws.onerror = function () {
-            console.log("Some Error occurred")
-        }
+const wss = new WebSocket.Server({ port: 8080 })
+
+wss.on("connection", ws => {
+    console.log("new client connected");
+    ws.on("message", data => {
+        console.log(`Client has sent us: ${data}`);
+
+        ws.send(JSON.stringify(model));
     });
-    console.log("The WebSocket server is running on port 8080");
+    ws.on("close", () => {
+        console.log("the client has connected");
+    });
+    ws.onerror = function () {
+        console.log("Some Error occurred")
+    }
+});
+console.log("The WebSocket server is running on port 8080");
 
 };
 main();
