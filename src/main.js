2e0328c351af3e2c639829462d5c541e7df26858
prometheanSolutions <promethean.solutions@proton.me>
2023-01-25 21:54:06 +0100

    convert to force graph

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
-import {
-  linkHorizontal, rgb, scaleOrdinal, schemeCategory10, select, zoom,
-} from 'd3';
+import * as d3 from 'd3';
 
 const margin = {
   top: 10, right: 10, bottom: 10, left: 10,
@@ -12,7 +10,7 @@ const width = 1200 - margin.left - margin.right;
 const height = 740 - margin.top - margin.bottom;
 
 // append the svg canvas to the page
-const svg = select('#chart').append('svg')
+const svg = d3.select('#chart').append('svg')
   .attr('width', '100%')
   .attr('height', '100%')
   .append('g')
@@ -22,45 +20,35 @@ const svg = select('#chart').append('svg')
   );
 
 const handleZoom = (e) => svg.attr('transform', e.transform);
-const zoomBehavior = zoom().on('zoom', handleZoom);
-select('svg').call(zoomBehavior);
+const zoomBehavior = d3.zoom().on('zoom', handleZoom);
+d3.select('svg').call(zoomBehavior);
 
-const color = scaleOrdinal(schemeCategory10);
+const color = d3.scaleOrdinal(d3.schemeCategory10);
 
 // load the data
 const generate = (model, settings) => {
-  const nodeMap = {};
+  console.log(model);
 
   const times = model.nodes.map((n) => n.tx.time);
   const timeMin = Math.min(...times);
   const timeMax = Math.max(...times);
   const timeInterval = timeMax - timeMin;
 
-  const graph = { nodes: {}, links: {} };
-  graph.nodes = model.nodes.map((n) => ({
-    ...n,
-    x: ((n.tx.time - timeMin) / timeInterval) * width,
-    y: n.tx.size % height,
-  }));
-  graph.nodes.forEach((x) => { nodeMap[x.name] = x; });
-  graph.links = model.links.map((x) => ({
-    source: nodeMap[x.source],
-    target: nodeMap[x.target],
-    value: x.value,
-    wallet: x.wallet,
-    info: x.info,
-  }));
+  const simulation = d3.forceSimulation()
+    .force('center', d3.forceCenter(width / 2, height / 2))
+    .force('charge', d3.forceManyBody())
+    .force('link', d3.forceLink().id((d) => d.id));
+  simulation.stop();
 
-  // add in the nodes
   const nodes = svg.append('g').selectAll('.node')
-    .data(graph.nodes)
+    .data(model.nodes)
     .enter()
-    .append('g')
-    .attr('class', 'node')
-    .attr('transform', (d) => `translate(${d.x},${d.y})`);
+    .append('g');
+    // .attr('transform', (d) => `translate(${d.x},${d.y})`);
 
   // add the rectangles for the nodes
   nodes.append('rect')
+    .attr('class', 'node_rect')
     .attr('height', 10)
     .attr('width', nodeWidth)
     .style('fill', () => 'gray')
@@ -69,28 +57,37 @@ const generate = (model, settings) => {
         window.open(settings['block-explorer-url'] + node.name, '_blank');
       }
     })
-    .style('stroke', (d) => rgb(d.color).darker(2))
+    .style('stroke', (d) => d3.rgb(d.color).darker(2))
     .append('title')
-    .text((d) => `${d.id}`);
-
-  const link = linkHorizontal()
-    .source((d) => ({
-      ...d.source,
-      x: d.source.x + nodeWidth,
-      y: d.source.y + 5,
-    }));
+    .text((d) => `${d.name}`);
 
   svg.append('g').selectAll('.link')
-    .data(graph.links)
-    .join('path')
-    .attr('d', link
-      .x((d) => d.x)
-      .y((d) => d.y))
-    .attr('fill', 'none')
+    .data(model.links)
+    .enter()
+    .append('line')
+    .attr('class', 'link_line')
     .attr('stroke-width', (d) => d.value * 20)
     .attr('stroke', (d) => color(d.wallet))
     .append('title')
     .text((d) => `${d.wallet}  ${JSON.stringify(d.info, null, 1)}  ${d.value}`);
+
+  simulation.nodes(model.nodes);
+  simulation.force('link').links(model.links);
+
+  simulation.on('tick', () => {
+  // position links
+    d3.selectAll('.link_line')
+      .attr('x1', (d) => d.source.x)
+      .attr('x2', (d) => d.target.x)
+      .attr('y1', (d) => d.source.y)
+      .attr('y2', (d) => d.target.y);
+
+    // position nodes
+    d3.selectAll('.node_rect')
+      .attr('x', (d) => d.x)
+      .attr('y', (d) => d.y);
+  });
+  simulation.alpha(1).restart();
 };
 
 const ws = new WebSocket('ws://localhost:8080');
