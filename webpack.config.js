ab0f72afeca29a5a7963388801825c195746ea27
prometheanSolutions <promethean.solutions@proton.me>
2023-01-28 10:25:27 +0100

    activate debugging

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,33 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
 
 const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 const templateParameters = require('./assets/parameters.json');
 
 module.exports = {
-  // Other rules...
-  plugins: [
-    new NodePolyfillPlugin(),
-  ],
-};
-
-module.exports = {
+  mode: 'development',
   target: 'web',
   entry: {
     app: ['./src/main.js'],
   },
   output: {
-    path: path.resolve(__dirname, '../build'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle-front.js',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'to-string-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              esModule: false,
+            },
+          }],
+      }],
   },
   devtool: 'inline-source-map',
   plugins: [
@@ -28,12 +36,20 @@ module.exports = {
       template: 'assets/index.html',
       templateParameters,
     }),
-    new Dotenv(),
   ],
 
+  watch: false,
   devServer: {
     static: './dist',
     port: 9000,
+  },
+  resolve: {
+    fallback: {
+      https: false,
+      http: false,
+      buffer: false,
+      fs: false,
+    },
   },
 
 };
